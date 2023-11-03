74022dbc58a1bf42bf34cc82461e6e0878095538
Danilo <danilo.silva192@icloud.com>
2023-11-03 17:51:04 -0300

    adiconada consulta de usuário

--- app/main.py
+++ app/main.py
@@ -12,3 +12,7 @@ app = FastAPI()
 
 
 app.include_router(user.router, prefix="/users", tags=["users"])
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,12 +1,15 @@
 from fastapi import APIRouter, Depends, HTTPException
+from db.sessions import get_db
 from sqlalchemy.orm import Session
+from crud.user import get_user
+
 
 router = APIRouter()
 
 
 @router.get("/users/{user_id}")
-async def read_user(user_id: int, db: Session = Depends(database.get_db)):
-    user = crud.get_user(db, user_id)
+async def read_user(user_id: int, db: Session = Depends(get_db)):
+    user = get_user(db, user_id)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return user
